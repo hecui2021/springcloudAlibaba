649da605bcdf168a0593f65161e601cd16853263
hecui <hecui0709@163.com>
2025-02-10 00:15:28 +0800

    netty

--- nettyStudy/src/main/java/com/study/netty/HelloClient.java
+++ nettyStudy/src/main/java/com/study/netty/HelloClient.java
@@ -1,5 +1,6 @@
 package com.study.netty;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -8,38 +9,38 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
 
 /**
  * @Author: peterche
  * @Description:
- * @Date: 2025/2/7 23:15
+ * @Date: 2025/2/9 21:36
  */
 @Slf4j
-public class HelloServer {
-    public static void main(String[] args) {
-        // 1.服务端启动器
-        new ServerBootstrap()
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 1.客户端启动器
+        new Bootstrap()
                 // 2.BossEventLoop,workerEventLoop
                 .group(new NioEventLoopGroup())
                 // 3.选择服务器的ServerSocketChannel实现
-                .channel(NioServerSocketChannel.class)
+                .channel(NioSocketChannel.class)
                 // 4.boss负责处理连接事件 worker(child)负责处理读写事件
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         // 添加具体的handle
-                        ch.pipeline().addLast(new StringDecoder());// 将bytebuf转为字符串（解码）
-                        // 自定义的handler
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-                            @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                log.info("channelRead msg:{}", msg);
-                            }
-                        });
+                        ch.pipeline().addLast(new StringEncoder());// 将字符串转为bytebuf（解码）
                     }
                 })
-                .bind(8080);
+                .connect(new InetSocketAddress("localhost",8080))
+                .sync()
+                .channel()
+                // 向服务器发送数据
+                .writeAndFlush("hello,world");
 
     }
 }
---
+++ nettyStudy/src/main/java/com/study/netty/module/TestEventLoop.java
@@ -0,0 +1,44 @@
+package com.study.netty.module;
+
+import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.NettyRuntime;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: peterche
+ * @Description:
+ * @Date: 2025/2/9 23:54
+ */
+@Slf4j
+public class TestEventLoop {
+    public static void main(String[] args) {
+        // 创建事件循环组
+        EventLoopGroup group = new NioEventLoopGroup(2);// io事件/普通任务/定时任务
+//        EventLoopGroup group = new DefaultEventLoopGroup();// 普通任务/定时任务
+
+        System.out.println(group.next());
+        System.out.println(group.next());
+        System.out.println(group.next());
+
+        // 普通任务
+//        group.next().execute(() -> {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            log.info("thread execute");
+//        });
+
+        // 定时任务
+        group.next().scheduleAtFixedRate(() -> {
+            log.info("thread execute2");
+        }, 3, 1, TimeUnit.SECONDS);
+
+        log.info("main");
+    }
+}
